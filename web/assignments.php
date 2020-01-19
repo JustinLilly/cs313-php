97d6da1554423d99b63e93d63a9229dfe6f29144
JustinLilly <lil16003@byui.edu>
2020-01-18 23:25:01 -0700

    added prove02

--- web/assignments.php
+++ web/assignments.php
@@ -2,8 +2,8 @@
 <html lang="en_us">
 <head>
   <meta charset="utf-8">
-  <title>Home page</title>
-  <meta name="description" content="cs313 home page">
+  <title>Assignments</title>
+  <meta name="description" content="cs313 assignments directory">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="author" content="Justin Lilly">
   <link href="/cs313-php/web/css/styles.css" rel="stylesheet" type="text/css" media="screen">
@@ -15,6 +15,9 @@
             <?php include $_SERVER ['DOCUMENT_ROOT'] . '/cs313-php/web/common/nav.php'; ?>
           </nav>
       </header>
+      <?php
+          echo "Coming soon!";
+      ?>
   </main>
 </body>
 </html>
---
+++ web/intro.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en_us">
+<head>
+  <meta charset="utf-8">
+  <title>Intro</title>
+  <meta name="description" content="cs313 introductory page">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Justin Lilly">
+  <link href="/cs313-php/web/css/styles.css" rel="stylesheet" type="text/css" media="screen">
+<body>
+  <main>
+      <header>
+          <?php include $_SERVER ['DOCUMENT_ROOT'] . '/cs313-php/web/common/header.php'; ?>
+          <nav id="page-nav">
+            <?php include $_SERVER ['DOCUMENT_ROOT'] . '/cs313-php/web/common/nav.php'; ?>
+          </nav>
+      </header>
+      
+      <section>
+        <h1>Justin Lilly</h1>
+      </section>
+      <p>
+        I am from Gilbert, Arizona. I come from a rather large family, being the seventh of nine kids. 
+        I served in the Philippines Cavite Mission. It was split from the Manila mission and was only six month old by the time I got there.
+        Because of said mission, it is my firm belief that the greatest fruit of all time is the mango. If the only mangoes you've ever had are the crap you find in grocery stores and Walmart, then you haven't had real mangoes. Real mangoes freshly plucked off the tree might possibly be sweeter than the fruit of the Tree of Life.
+      </p>
+      <img src="/cs313-php/web/css/mangoes.jpg" alt="mangoes">
+  </main>
+</body>
+</html>
