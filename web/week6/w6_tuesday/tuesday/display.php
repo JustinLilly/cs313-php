e65e0324f1464890f9ff258ed8f2f90c763eafd0
JustinLilly <lil16003@byui.edu>
2020-02-11 16:05:35 -0700

    w6 class example fix

--- web/week6/w6_tuesday/tuesday/display.php
+++ web/week6/w6_tuesday/tuesday/display.php
@@ -11,23 +11,19 @@
             $statement->execute();
             while ($row = $statement->fetch(PDO::FETCH_ASSOC))
             {
-               $id = $row['id'];
-               $first = $row['first_name'];
-               $last = $row['last_name'];
-               $food = $row['food_type'];
+               $id      = $row['id'];
+               $first   = $row['first_name'];
+               $last    = $row['last_name'];
+               $food_id = $row['food_type'];
 
-               $foods = $db->prepare("SELECT food FROM w6_food WHERE Id = $food_id");
+               $foods = $db->prepare("SELECT food FROM w6_food WHERE ID = $food_id");
                $foods->execute();
-               while ($fRow = $statement->fetch(PDO::FETCH_ASSOC)){
+               while ($fRow = $foods->fetch(PDO::FETCH_ASSOC))
+               {
                   $food = $fRow['food'];
                }
                echo "<h1>$first $last's favorite food is $food</h1>";
             }
-
-            // retrieve url parameter
-            // execute query to pull up data from that id
-            // execute another query to get food data
-            // display name and favorite food
          ?>
 
 		</div>
--- web/week6/w6_tuesday/tuesday/index.php
+++ web/week6/w6_tuesday/tuesday/index.php
@@ -1,28 +1,7 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <?php
-   $first = $_POST['first'];
-   $last = $_POST['last'];
-   $food = $_POST['food'];
-
 	require("dbConnect.php");
-   $db = get_db();
-   
-   try{
-      $query = 'INSERT INTO w6_user (first_name, last_name, foot_type) VALUES (:first, :last, 1)';
-      $statement = $db->prepare($query);
-      $statement->bindValue(':first', $first);
-      $statement->bindValue(':last', $last);
-      $statement->bindValue(':food', $food);
-      $statement->execute();
-
-      $userId = $db->lastInsertId("w6_user_id_seq");
-   }
-   catch(Exception $ex) {
-      echo "Error with DB. Details: $ex";
-      die();
-   }
-   header("Location: display.php/?personId=$userID");
-   die();
+	$db = get_db();
 ?>
 <body>
    <div class="container" style="margin-top:50px;">
@@ -39,14 +18,12 @@
                      <?php
                         $statement = $db->prepare("SELECT * FROM w6_food");
                         $statement->execute();
-                        while ($row = $statement->fetch(PDO::FETCH_ASSOC)){
-                           $id = $row['id'];
+                        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+                        {
+                           $id   = $row['id'];
                            $food = $row['food'];
                            echo "<option value='$id'>$food</option>";
                         }
-                        // query db
-                        // loop through results
-                        // output html option for each row
                      ?>
                   </select>
                </div>
